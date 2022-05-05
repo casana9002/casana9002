eeee5269a19a3617007dd71016e944a21181e063
casana9002 <92309356+casana9002@users.noreply.github.com>
2022-05-05 12:24:11 -0400

    Update MainActivity.java
    Mensaje de prueba para probar desde la web

--- app/src/main/java/appproduccion/xetid/cu/MainActivity.java
+++ app/src/main/java/appproduccion/xetid/cu/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //comentario de prueba para git
+        //comentario de prueba para git desde la web
     }
 }
